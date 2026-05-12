f249a1a7e4af28cc49a4e967eebbd3a3f399c501
Dylan <galaxy.warship5r@icloud.com>
2026-05-12 07:31:55 -0700

    Temporarily remove Daily SDK package dependency

--- Package.swift
+++ Package.swift
@@ -10,15 +10,13 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/MailCore/mailcore2", branch: "master"),
         .package(url: "https://github.com/appwrite/sdk-for-apple", from: "16.1.0"),
-        .package(url: "https://github.com/daily-co/daily-client-ios.git", from: "0.37.0")
     ],
     targets: [
         .executableTarget(
             name: "ToolsKit",
             dependencies: [
                 .product(name: "MailCore2", package: "mailcore2"),
-                .product(name: "Appwrite", package: "sdk-for-apple"),
-                .product(name: "Daily", package: "daily-client-ios")
+                .product(name: "Appwrite", package: "sdk-for-apple")
             ],
             path: "Sources"
         )
